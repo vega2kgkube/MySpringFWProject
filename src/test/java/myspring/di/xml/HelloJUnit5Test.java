fd5428971de6cc1bce88b717651e8df30e43f3d2
vega2kgkube <vega2kgkube@gmail.com>
2025-08-08 16:33:00 +0900

    JunitTest

--- src/test/java/myspring/di/xml/HelloJUnit5Test.java
+++ src/test/java/myspring/di/xml/HelloJUnit5Test.java
@@ -1,5 +1,6 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -13,5 +14,11 @@ public class HelloJUnit5Test {
 		//2. Container가 생성한 Hello 스프링빈을 요청하기
 		Hello helloById = (Hello)context.getBean("hello");
 		Hello helloByType = context.getBean("hello", Hello.class);
+		
+		//주소를 비교해서 Singleton 인지 확인하기
+		System.out.println(helloById == helloByType);
+		//Assertions.assertSame() 사용
+		Assertions.assertSame(helloById, helloByType);
+		
 	}
 }
