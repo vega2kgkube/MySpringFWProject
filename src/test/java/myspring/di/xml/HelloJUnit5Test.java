0e0a9fa7d2c949f5aa34b9e29018a527d40fc2bd
vega2kgkube <vega2kgkube@gmail.com>
2025-08-08 16:05:00 +0900

    JunitTest

--- src/test/java/myspring/di/xml/HelloJUnit5Test.java
+++ src/test/java/myspring/di/xml/HelloJUnit5Test.java
@@ -1,11 +1,14 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloJUnit5Test {
 
 	@Test
 	void helloBean() {
-		
+		//1. Container 객체생성
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:spring-beans.xml");
 	}
 }
