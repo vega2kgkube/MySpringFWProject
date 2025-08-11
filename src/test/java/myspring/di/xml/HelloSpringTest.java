037e6808798c8d564d7e01a47592bd845eb24789
vega2kgkube <vega2kgkube@gmail.com>
2025-08-11 09:36:34 +0900

    @ContextConfiguration

---
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -0,0 +1,11 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring-beans.xml")
+public class HelloSpringTest {
+
+}
