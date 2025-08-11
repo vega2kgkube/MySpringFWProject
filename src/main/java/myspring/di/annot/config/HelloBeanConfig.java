72370dfecc21e7039c21127ce3a0fd98ad7d8119
vega2kgkube <vega2kgkube@gmail.com>
2025-08-11 15:37:43 +0900

    HelloBeanConfig add

---
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -0,0 +1,11 @@
+package myspring.di.annot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//설정클래스임을 나타내는 어노테이션
+@Configuration
+@ComponentScan
+public class HelloBeanConfig {
+
+}
