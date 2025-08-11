2f4214ff5260ed8da50c4622f66bf3ee9b1f9ea4
vega2kgkube <vega2kgkube@gmail.com>
2025-08-11 09:41:01 +0900

    @Autowired

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,11 +1,15 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class HelloSpringTest {
-
+	@Autowired
+	Hello hello;
+	
+	
 }
